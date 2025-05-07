38a3d6b17ad7188d60ef037ce206b81a40f4701f
NervzZ <julien.jordan@epfl.ch>
2025-05-07 23:02:28 +0200

    Some script edit based on PR comments

--- Assets/Scripts/Debug/CanvasLogger.cs
+++ Assets/Scripts/Debug/CanvasLogger.cs
@@ -1,30 +1,27 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.Serialization; // Remove if you use UnityEngine.UI.Text
 
 /// <summary>
-/// Code généré pour afficfher la console sur un canvas pour aider au debug en étant en jeu
+/// Code généré pour afficher la console sur un canvas pour aider au debug en étant en jeu
+/// TODO : A supprimer plus tard car utilisé que pour des tests
 /// </summary>
 public class UIConsoleLogger : MonoBehaviour
 {
-    [FormerlySerializedAs("_output")]
     [Header("Drag your Text or TMP_Text here")]
     [SerializeField]
     private TMP_Text output;       // Or: public Text _output;
 
-    readonly System.Collections.Generic.List<string> _logs = new();
-
-    void OnEnable()
+    private void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         Application.logMessageReceived -= HandleLog;
     }
 
-    void HandleLog(string logString, string stackTrace, LogType type)
+    private void HandleLog(string logString, string stackTrace, LogType type)
     {
         // Directly set the UI text to the latest log entry
         output.text = logString;
