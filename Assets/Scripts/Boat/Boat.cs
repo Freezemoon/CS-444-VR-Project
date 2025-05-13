c4e90b8af5c106fe0c507225ac48761dab530fe9
Oliver Sorrentino <46000166+oliver216@users.noreply.github.com>
2025-05-13 23:35:20 +0200

    4-boat-creation (#30)
    * Create Boat
    
    Add the boat prefab with a teleportation zone and a basic script to keep player on it
    
    * Correct prefab boat
    
    correct teleportation zone, but player still don't stay in it
    
    * Correct bug, and add boat on demo scène
    
    * Add scene and improve boat
    
    * Add Boat Handle, Add modified boat model
    
    * Small correction
    
    * Try to correct bug lever
    
    * Add button and boat handle
    
    * Handle working
    
    * Add button finish but not working
    
    * Add button working
    
    * Boat Advance, turn, reverse, on/off
    
    Knowed probleme, Boat stop turning at a max angle of aprox 720 degres, Input from controller not detected
    
    * Unity correction
    
    * Update gitignore
    
    * Add rigid body to boat, separate walking zone and teleportation zone of the boat
    
    * Correct rotation bug and change sense
    
    * Add sound to button
    
    * Clean code
    
    * Remove useless component (Counter of button)
    
    * Try to solved handle problem without real succes
    
    * Handle working !
    
    * Avancement Script boat speed
    
    * Press when holding the handle move the boat, clean some file and code
    
    * Save
    
    * Handle working again
    
    * Work somehow nice
    
    * HandleButton work
    
    still trembling and recoil when accelerate to fast
    
    * Imrpove script for boathandle
    
    * Clean script, test on VR headset, change boat value
    
    * Add motor sound, smooth acceleration and deceleration
    
    * Save prefab
    
    * Add indicator lamp for button
    
    * Reduce light intensity
    
    * Correct button light, change mesh collidor
    
    * Add lake boarder collision management for the boat, minor boat refactor
    
    * Update prefabs related to boat and small refactor of project folders
    
    * Update package
    
    ---------
    
    Co-authored-by: Alexander Wohlfahrt <akawohlfahrt@gmail.com>

---
+++ Assets/Scripts/Boat/Boat.cs
@@ -0,0 +1,164 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    public Transform leverTransform; // Reference to the handle
+    public float speedMultiplier = 3f; // Multiplie the boat speed
+    public float turnSensitivity = 0.25f; // Boat turn sensitivity
+    public float accelerationRate = 1.5f;
+
+    public AudioClip engineStartClip;
+    public AudioClip engineLoopClip;
+    private AudioSource engineAudioSource;
+
+    public GameObject OnOffIndicatorLamp;
+    public GameObject reverseIndicatorLamp;
+    
+    private bool engine = false;
+    private int reverse = 1; // 1 = forward, -1 = reverse
+    private float speed = 0f;
+    private float currentSpeed = 0f;
+    
+    // Fields for collision-based movement restriction
+    private float pushForce = 1.2f;
+    private List<Vector3> activePushDirections = new List<Vector3>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        leverTransform.GetComponent<BoatHandle>().OnThrottleChanged += UpdateThrottle;
+
+        OnOffIndicatorLamp.GetComponent<MeshRenderer>().enabled = false;
+        reverseIndicatorLamp.GetComponent<MeshRenderer>().enabled = false;
+
+        engineAudioSource = gameObject.AddComponent<AudioSource>();
+        engineAudioSource.loop = true;
+        engineAudioSource.playOnAwake = false;
+        engineAudioSource.clip = engineLoopClip;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Quaternion boatToHandle = Quaternion.Inverse(transform.rotation) * leverTransform.rotation;
+        float localAngle = boatToHandle.eulerAngles.y;
+        if (localAngle > 180f) localAngle -= 360f;
+        if (engine == true)
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, speed, accelerationRate * Time.deltaTime);
+            
+            Vector3 moveDirection = -transform.right * reverse;
+            
+            if (activePushDirections.Count > 0)
+            {
+                Vector3 totalPush = Vector3.zero;
+                foreach (var dir in activePushDirections)
+                {
+                    totalPush += dir;
+                }
+
+                totalPush = totalPush.normalized;
+                moveDirection += totalPush * pushForce;
+                moveDirection = moveDirection.normalized;
+            }
+
+            transform.position += moveDirection * currentSpeed * Time.deltaTime;
+
+            if (currentSpeed > 0f)
+            {
+                float turnSpeed = localAngle * turnSensitivity; // How fast the boat turns
+                transform.Rotate(0, - turnSpeed * Time.deltaTime, 0);
+            }
+            if (speed == 0f)
+            {
+                engineAudioSource.volume = 0.7f;
+                engineAudioSource.pitch = 1f;
+            }
+            else if (reverse == -1)
+            {
+                engineAudioSource.volume = 1f;
+                engineAudioSource.pitch = 0.7f;
+            }
+            else
+            {
+                engineAudioSource.volume = 1f;
+                engineAudioSource.pitch = 1.5f;
+            }
+        }
+    }
+
+    private void UpdateThrottle(float value)
+    {
+        speed = value * speedMultiplier; // Value goes from 0 to 1 based on trigger press
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LakeBorder"))
+        {
+            Vector3 contactPoint = other.ClosestPoint(transform.position);
+            Vector3 dir = (transform.position - contactPoint).normalized;
+            activePushDirections.Add(dir);
+        }
+        else if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(transform);
+            Debug.Log("Player entered the boat trigger zone");
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("LakeBorder"))
+        {
+            Vector3 contactPoint = other.ClosestPoint(transform.position);
+            Vector3 dir = (transform.position - contactPoint).normalized;
+
+            // Remove approximate match from list
+            activePushDirections.RemoveAll(d => Vector3.Angle(d, dir) < 10f); // Angle threshold to remove correct one
+        }
+        else if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+            Debug.Log("Player exited the boat trigger zone");
+        }
+    }
+    
+    public void EngineButtonOn()
+    {
+        if (engine == false)
+        {
+            engine = true; // Turn on the engine
+            OnOffIndicatorLamp.GetComponent<MeshRenderer>().enabled = true;
+
+            AudioSource.PlayClipAtPoint(engineStartClip, transform.position);
+            engineAudioSource.Play();
+            Debug.Log("Engine is on and reverse is off"); 
+        }
+        else
+        {
+            engine = false;
+            OnOffIndicatorLamp.GetComponent<MeshRenderer>().enabled = false;
+
+            engineAudioSource.Stop();
+            Debug.Log("Engine is off"); 
+        }
+    }
+
+    public void EngineButtonReverse()
+    {
+        if (reverse == 1)
+        {
+            reverse = -1; // Turn on the reverse
+            reverseIndicatorLamp.GetComponent<MeshRenderer>().enabled = true;
+            Debug.Log("Reverse is on");
+        }
+        else
+        {
+            reverse = 1;
+            reverseIndicatorLamp.GetComponent<MeshRenderer>().enabled = false;
+            Debug.Log("Reverse is off"); 
+        }
+    }
+}
---
+++ Assets/Scripts/Boat/BoatHandle.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class BoatHandle : MonoBehaviour
+{
+    public Transform boat;
+    public Transform handlePivot;
+    public XRGrabInteractable grabInteractable;
+    public InputActionReference throttleAction; // Trigger button
+    public Transform throttleButtonVisual; // Visual button to move
+    public float rotationSpeed = 250f;
+    public float maxRotation = 45f;
+    public float maxButtonYMovement = 0.2f;
+    public System.Action<float> OnThrottleChanged; // Notify the Boat script
+
+    private Transform grabbingHand;
+    private float currentYRotation;
+    private Vector3 localOffset;
+    private float triggerValue;
+    private float initialGrabAngle;
+    private float initialHandleRotation;
+    private Vector3 buttonPosition;
+
+    private void Start()
+    {
+        localOffset = boat.InverseTransformPoint(transform.position);
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+        throttleAction.action.Enable();
+
+        buttonPosition = throttleButtonVisual.localPosition;
+    }
+
+    private void OnDestroy()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrab);
+        grabInteractable.selectExited.RemoveListener(OnRelease);
+        throttleAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        if (grabbingHand == null)
+        {
+            transform.rotation = Quaternion.Euler(0f, currentYRotation, 0f) * boat.rotation;
+            transform.position = boat.TransformPoint(localOffset);
+
+            triggerValue = 0f;
+            OnThrottleChanged?.Invoke(triggerValue);
+
+            throttleButtonVisual.localPosition = buttonPosition;
+            return;
+        }
+
+        Vector3 handLocal = boat.InverseTransformPoint(grabbingHand.position);
+        Vector3 pivotLocal = boat.InverseTransformPoint(handlePivot.position);
+        Vector3 dir = handLocal - pivotLocal;
+
+        float currentAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        float deltaAngle = currentAngle - initialGrabAngle;
+        float targetRotation = initialHandleRotation + deltaAngle;
+
+        currentYRotation = Mathf.MoveTowards(currentYRotation, Mathf.Clamp(targetRotation, - maxRotation, maxRotation), rotationSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0f, currentYRotation, 0f) * boat.rotation;
+        transform.position = boat.TransformPoint(localOffset);
+
+        triggerValue = throttleAction.action.ReadValue<float>(); // 0 to 1
+        OnThrottleChanged?.Invoke(triggerValue); // Send value to the Boat script
+
+        Vector3 buttonPositionTemp = buttonPosition;
+        buttonPositionTemp.y = buttonPosition.y - (triggerValue * maxButtonYMovement);
+        throttleButtonVisual.localPosition = buttonPositionTemp;
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        grabbingHand = args.interactorObject.transform;
+
+        Vector3 handLocal = boat.InverseTransformPoint(grabbingHand.position);
+        Vector3 pivotLocal = boat.InverseTransformPoint(handlePivot.position);
+        Vector3 dir = handLocal - pivotLocal;
+
+        initialGrabAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        initialHandleRotation = currentYRotation;
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        grabbingHand = null;
+    }
+}
