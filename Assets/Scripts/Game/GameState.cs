2c4020fc9c6b50ef4ae9dd6ab01f881e9ece0576
Freezemoon <freez3moon@gmail.com>
2025-05-17 13:47:11 +0200

    added game states for bait components

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -10,5 +10,12 @@ namespace Game
         public int BucketValue = 20; // valeur des poissons pas encore cash in
         public int Money = 0; // monnaie actuelle en posession du joueur
         public FishingRodStats CurrentRod = FishingRodLibrary.Basic; // Canne actuelle du joueur
+        public int Component1Amount = 0;
+        public int Component2Amount = 0;
+        public int Component3Amount = 0;
+        public int Component4Amount = 0;
+        public int Component5Amount = 0;
+        public int Component6Amount = 0;
+        public int DynamiteAmount = 0;
     }
 }
