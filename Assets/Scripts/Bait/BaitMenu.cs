6710e57285ac06807f06303ecd2fb10fbbef7208
NervzZ <julien.jordan@epfl.ch>
2025-05-24 06:58:48 +0200

    Inventory now tracking caught fish and money states

--- Assets/Scripts/Bait/BaitMenu.cs
+++ Assets/Scripts/Bait/BaitMenu.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Net.Mime;
 using Game;
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class BaitMenu : MonoBehaviour
 {
+    [Header("GameState texts")]
+    [Tooltip("The TextMeshPro text to display fish tracking and money.")]
+    [SerializeField] private TextMeshProUGUI moneyText;
+    [SerializeField] private TextMeshProUGUI fishTrackerText;
+    
     [Header("Buttons")]
     [Tooltip("The button prefabs for inventory elements.")]
     [SerializeField] private GameObject baitButton1;
@@ -87,6 +94,17 @@ public class BaitMenu : MonoBehaviour
 
     private void UpdateUI()
     {
+        int money = GameManager.instance.GetMoney();
+        int bucketValue = GameManager.instance.GetBucketValue();
+        int easyFishCount = GameManager.instance.State.EasyFishCought;
+        int mediumFishCount = GameManager.instance.State.MediumFishCought;
+        int hardFishCount = GameManager.instance.State.HardFishCought;
+
+        moneyText.text = $"Money: {money}<color=#FFD700>G</color>    In Bucket: {bucketValue}<color=#FFD700>G</color>";
+        fishTrackerText.text = $"Fish tracker: {easyFishCount}x <color=blue>Easy</color>, " +
+                               $"{mediumFishCount}x <color=green>Medium</color>, " +
+                               $"{hardFishCount}x <color=red>Hard</color>";
+        
         baitButton1.SetActive(GameManager.instance.State.BBBaitCount > 0);
         baitButton2.SetActive(GameManager.instance.State.BGBaitCount > 0);
         baitButton3.SetActive(GameManager.instance.State.BRBaitCount > 0);
